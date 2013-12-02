e2c1d8b2995367902eb70a2ad8c0dc42cdf03944
Sytse Sijbrandij <sytse@dosire.com>
2013-12-02 12:20:43 +0100

    Move to official cookbook.

--- site-cookbooks/gitlab/metadata.rb
+++
@@ -1,7 +0,0 @@
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-%w{ git redisio build-essential python readline openssl openssh perl xml zlib rbenv }.each do |cb_depend|
-  depends cb_depend
-end
-%w{ debian ubuntu }.each do |os|
-  supports os
-end
